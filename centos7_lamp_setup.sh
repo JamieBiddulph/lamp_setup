f989875fcae9d9cd9bf956eeb3545d1ba80cd9ad
Jamie <jamiebiddulph25@gmail.com>
2018-11-07 19:56:38 +0000

    update mariadb version to 10.3

--- centos7_lamp_setup.sh
+++ centos7_lamp_setup.sh
@@ -12,12 +12,12 @@ yum install -y httpd
 systemctl start httpd.service
 #enable apache
 systemctl enable httpd.service
-#Get mariadb 10.2
-echo "# MariaDB 10.2 CentOS repository list - created 2017-10-10 21:08 UTC
+#Get mariadb 10.3
+echo "# MariaDB 10.3 CentOS repository list - created 2018-11-07 19:52 UTC
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.2/centos7-amd64
+baseurl = http://yum.mariadb.org/10.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1" > /etc/yum.repos.d/MariaDB.repo
 #Install MariaDB
